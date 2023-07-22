10e01a3f295705056ddbac82fa345e0f46987652
amy_pr <ampreci@gmail.com>
2023-07-22 10:45:36 -0700

    add Card component with jsx and placeholder text

---
+++ src/Card.jsx
@@ -0,0 +1,28 @@
+
+function Card () {
+
+    return (
+        <>
+            <div id="resourcesContainer"> 
+                       
+                <article class="repo card-1" id="resource">
+                    <h3><a id="title" href="" target="_blank">Resource Title</a></h3>
+                    <p id="description">This would be my one to two line description of the website</p>
+
+                    <div id="tagsContainer">
+                        <span class="tag">videos</span>
+                        <span class="tag">language</span>
+                        <span class="tag">pragmatics</span>
+                    </div>
+                </article>
+                        
+            </div>
+        </>
+    )
+
+
+
+
+}
+
+export default Card
